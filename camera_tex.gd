fa4c16c263c467685a708692be3f7d01898fd802
C0desnake <tychojason@gmail.com>
2024-09-25 16:26:04 +0200

    -

--- camera_tex.gd
+++ camera_tex.gd
@@ -1,24 +1,35 @@
 extends TextureRect
 
-@export var debug_Label: Label
-
 var camera: CameraFeed
 
+@onready var DebugLabel: Label = $"../Label"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	debug_Label.text = "start up"
-	if CameraServer.feeds():
-		camera = CameraServer.feeds()[0]
-		debug_Label.text = "camera: " + str(camera)
+	var debug_text = "cameras:\n"
+	for feed in CameraServer.feeds():
+		var name = feed.get_name()
+		debug_text += name + "\n"
+		
+		# if camera_name is left empty, use the first available camera
+		if camera == null:
+			camera = feed
+	
+	if camera == null:
+		debug_text = "no cameras"
+		return
+	
+	else:
 		camera.feed_is_active = true
 		
-		var cam_tex_y = material.get_shader_material("camera_y")
-		var cam_tex_CbCr = material.get_shader_material("camera_CbCr")
+		var cam_tex_y = material.get_shader_parameter("camera_y")
+		var cam_tex_CbCr = material.get_shader_parameter("camera_CbCr")
 		
 		cam_tex_y.camera_feed_id = camera.get_id()
 		cam_tex_CbCr.camera_feed_id = camera.get_id()
 		
-		material.set_shader_material("camera_y", cam_tex_y)
-		material.set_shader_material("camera_CbCr", cam_tex_CbCr)
+		material.set_shader_parameter("camera_y", cam_tex_y)
+		material.set_shader_parameter("camera_CbCr", cam_tex_CbCr)
+	
+	DebugLabel.text = str(debug_text)
 	
